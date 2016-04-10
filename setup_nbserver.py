a314169ed0e668e9d82b1978788c7860ba7b2294
Dong-Hyun Kwak <imcomking@gmail.com>
2016-04-11 00:03:41 +0900

    Update setup_nbserver.py

--- setup_nbserver.py
+++ setup_nbserver.py
@@ -12,7 +12,7 @@ yes = set(['yes','y', 'ye', ''])
 no = set(['no','n'])
 home_dir = os.environ['HOME']
 
-print ("Do you want to reset password? (y/n)")
+print ("Do you want a password? (y/n)")
 choice = raw_input().lower()
 if choice in yes:
     from IPython.lib import passwd
@@ -21,12 +21,12 @@ if choice in yes:
     config_str = """
 # Server config
 c = get_config()
+c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = u'{}'
-c.NotebookApp.ip = '*'
 # It is a good idea to put it on a known, fixed port
 c.NotebookApp.port = 8888
-c.NotebookApp.notebook_dir = u'/home/biadmin/'
+c.NotebookApp.notebook_dir = u'/'
 """.format(pwsha)
 
     with open(home_dir+"/.ipython/profile_nbserver/ipython_notebook_config.py", "w") as cf:
@@ -34,9 +34,8 @@ c.NotebookApp.notebook_dir = u'/home/biadmin/'
         
         
 #elif choice in no:
-'''
+
 else:
-   #print("Please respond with 'y' or 'n'")
 
     config_str = """
 # Server config
@@ -45,13 +44,13 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 # It is a good idea to put it on a known, fixed port
 c.NotebookApp.port = 8888
-c.NotebookApp.notebook_dir = u'/home/biadmin/'
+c.NotebookApp.notebook_dir = u'/'
 """
-'''
 
 
 
-os.system("screen -dRR -dmS ipython_notebook ipython notebook --profile=nbserver;")
+# this is usually make some error... do not use this!
+#os.system("screen -dRR -dmS ipython_notebook ipython notebook --profile=nbserver;")
 
 # https://www.gnu.org/software/screen/manual/screen.html
 # screen install check
